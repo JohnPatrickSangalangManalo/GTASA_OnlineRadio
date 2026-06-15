af7b2909b4eb967a9ebdf9a8da507ee32a182106
John Patrick <80339143+JohnPatrickSangalangManalo@users.noreply.github.com>
2026-06-15 10:45:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <mod/amlmod.h>
 #include <mod/logger.h>
 #include <mod/config.h>
+
 #include <thread>
 #include <cmath>
-#include <dlfcn.h>
 
 #include <Events.h>
 #include <engine/RsGlobal.h>
@@ -23,7 +23,7 @@ NEEDGAME(com.rockstargames.gtasa)
 uintptr_t pGTASA = 0;
 void* hGTASA = NULL;
 
-// ===================== CONFIG =====================
+// ================= CONFIG =================
 ConfigEntry* pCurrentRadioIndex;
 ConfigEntry* pRadioVolume;
 
@@ -32,14 +32,22 @@ const char** pRadioNames;
 
 int nRadiosCount = 0;
 int nRadioIndex = 0;
-
 uint32_t pCurrentRadio = 0;
+
 bool bRadioPlaying = false;
 
-// ===================== IMGUI =====================
-bool g_showRadioUI = true;
+// ================= IMGUI SAFETY =================
+// Prevent compile crash if ImGui is missing
+#ifdef IMGUI_VERSION
+#include <imgui.h>
+#define HAS_IMGUI 1
+#else
+#define HAS_IMGUI 0
+#endif
 
-// ===================== HELPERS =====================
+bool g_showUI = true;
+
+// ================= PLAYBACK =================
 void PlayStation(int index)
 {
     if(index < 0 || index >= nRadiosCount) return;
@@ -75,53 +83,102 @@ void PlayStation(int index)
             bRadioPlaying = true;
         }
     }
-    else
-    {
-        logger->Error("Failed to open stream: %d", BASS->ErrorGetCode());
-    }
 }
 
 void NextStation()
 {
-    int idx = nRadioIndex + 1;
-    if(idx >= nRadiosCount) idx = 0;
-    PlayStation(idx);
+    PlayStation((nRadioIndex + 1) % nRadiosCount);
 }
 
 void PrevStation()
 {
-    int idx = nRadioIndex - 1;
-    if(idx < 0) idx = nRadiosCount - 1;
-    PlayStation(idx);
+    PlayStation((nRadioIndex - 1 + nRadiosCount) % nRadiosCount);
 }
 
-void SetVolume(int vol)
+// ================= VOLUME CALLBACK (FIXED) =================
+void SetVolume(int oldVal, int newVal, void* data)
 {
     if(pCurrentRadio)
-        BASS->ChannelSetAttribute(pCurrentRadio, BASS_ATTRIB_VOL, 0.005f * vol);
+        BASS->ChannelSetAttribute(pCurrentRadio, BASS_ATTRIB_VOL, 0.005f * newVal);
 
-    pRadioVolume->SetInt(vol);
+    pRadioVolume->SetInt(newVal);
     cfg->Save();
 }
 
-// ===================== GAME HOOKS =====================
-DECL_HOOK(void, PauseGame, void* self)
+// ================= SAFE VEHICLE CHECK =================
+uintptr_t GetPlayerVeh()
 {
-    if(pCurrentRadio)
-        BASS->ChannelPause(pCurrentRadio);
-
-    return PauseGame(self);
+    return sautils ? sautils->GetPlayerVehicle(-1) : 0;
 }
 
-DECL_HOOK(void, ResumeGame, void* self)
+// ================= IMGUI UI =================
+void DrawUI()
 {
-    if(pCurrentRadio && !CTimer::IsPaused())
-        BASS->ChannelPlay(pCurrentRadio, false);
+#if HAS_IMGUI
+    if(!g_showUI) return;
 
-    return ResumeGame(self);
+    ImGui::Begin("🎵 Online Radio", &g_showUI);
+
+    ImGui::Text("Station: %s", pRadioNames[nRadioIndex]);
+    ImGui::Separator();
+
+    if(ImGui::BeginListBox("Stations"))
+    {
+        for(int i = 0; i < nRadiosCount; i++)
+        {
+            bool selected = (i == nRadioIndex);
+            if(ImGui::Selectable(pRadioNames[i], selected))
+                PlayStation(i);
+        }
+        ImGui::EndListBox();
+    }
+
+    ImGui::Separator();
+
+    if(ImGui::Button("⏮ Prev")) PrevStation();
+    ImGui::SameLine();
+    if(ImGui::Button(bRadioPlaying ? "⏸ Pause" : "▶ Play"))
+    {
+        if(bRadioPlaying && pCurrentRadio)
+        {
+            BASS->ChannelPause(pCurrentRadio);
+            bRadioPlaying = false;
+        }
+        else
+        {
+            PlayStation(nRadioIndex);
+        }
+    }
+    ImGui::SameLine();
+    if(ImGui::Button("⏭ Next")) NextStation();
+
+    ImGui::Separator();
+
+    int vol = pRadioVolume->GetInt();
+    if(ImGui::SliderInt("Volume", &vol, 0, 100))
+        SetVolume(pRadioVolume->GetInt(), vol, nullptr);
+
+    ImGui::Text("Status: %s", bRadioPlaying ? "Playing" : "Stopped");
+
+    ImGui::End();
+#endif
 }
 
-// ===================== STOP RADIO =====================
+// ================= RADIO THREAD =================
+void StartRadioThread()
+{
+    PlayStation(nRadioIndex);
+}
+
+// ================= HOOKS =================
+DECL_HOOK(void, StartRadio, uintptr_t self, uintptr_t vehicleInfo)
+{
+    if(GetPlayerVeh() != 0)
+        std::thread(StartRadioThread).detach();
+
+    StartRadio(self, vehicleInfo);
+}
+
 DECL_HOOK(void, StopRadio, uintptr_t self, uintptr_t vehicleInfo, unsigned char flag)
 {
     if(pCurrentRadio)
@@ -133,112 +190,23 @@ DECL_HOOK(void, StopRadio, uintptr_t self, uintptr_t vehicleInfo, unsigned char 
 
     bRadioPlaying = false;
 
-    return StopRadio(self, vehicleInfo, flag);
+    StopRadio(self, vehicleInfo, flag);
 }
 
-// ===================== START RADIO =====================
-void StartRadioThread()
-{
-    PlayStation(nRadioIndex);
-}
-
-DECL_HOOK(void, StartRadio, uintptr_t self, uintptr_t vehicleInfo)
-{
-    if(FindPlayerVehicle(-1, false) != 0)
-        std::thread(StartRadioThread).detach();
-}
-
-// ===================== IMGUI UI =====================
-void DrawRadioUI()
-{
-    if(!g_showRadioUI) return;
-
-    ImGui::Begin("🎵 Online Radio", &g_showRadioUI);
-
-    ImGui::Text("Station: %s", pRadioNames[nRadioIndex]);
-    ImGui::Separator();
-
-    // Station list
-    if(ImGui::BeginListBox("Stations"))
-    {
-        for(int i = 0; i < nRadiosCount; i++)
-        {
-            bool selected = (i == nRadioIndex);
-            if(ImGui::Selectable(pRadioNames[i], selected))
-            {
-                PlayStation(i);
-            }
-        }
-        ImGui::EndListBox();
-    }
-
-    ImGui::Separator();
-
-    // Controls
-    if(ImGui::Button("⏮ Prev"))
-        PrevStation();
-
-    ImGui::SameLine();
-
-    if(ImGui::Button(bRadioPlaying ? "⏸ Stop" : "▶ Play"))
-    {
-        if(bRadioPlaying)
-        {
-            if(pCurrentRadio)
-                BASS->ChannelPause(pCurrentRadio);
-
-            bRadioPlaying = false;
-        }
-        else
-        {
-            PlayStation(nRadioIndex);
-        }
-    }
-
-    ImGui::SameLine();
-
-    if(ImGui::Button("⏭ Next"))
-        NextStation();
-
-    ImGui::Separator();
-
-    // Volume
-    int vol = pRadioVolume->GetInt();
-    if(ImGui::SliderInt("Volume", &vol, 0, 100))
-    {
-        SetVolume(vol);
-    }
-
-    ImGui::Separator();
-
-    ImGui::Text("Status: %s",
-        bRadioPlaying ? "Playing" : "Stopped"
-    );
-
-    ImGui::End();
-}
-
-// ===================== MOD LOAD =====================
+// ================= MOD LOAD =================
 ON_MOD_LOAD()
 {
     pGTASA = aml->GetLib("libGTASA.so");
     hGTASA = aml->GetLibHandle("libGTASA.so");
 
     BASS = (IBASS*)GetInterface("BASS");
-    BASS->SetConfig(BASS_CONFIG_NET_TIMEOUT, 5000);
+    sautils = (ISAUtils*)GetInterface("SAUtils");
 
-    // config
     pCurrentRadioIndex = cfg->Bind("CurrentRadioIndex", 0);
     pRadioVolume = cfg->Bind("RadioVolume", 80);
 
     nRadiosCount = cfg->Bind("RadiosCount", 0)->GetInt();
     if(nRadiosCount > MAX_RADIOS) nRadiosCount = MAX_RADIOS;
-
-    if(nRadiosCount <= 0)
-    {
-        logger->Error("No radios found in config!");
-        return;
-    }
 
     pRadioStreams = new const char*[nRadiosCount];
     pRadioNames   = new const char*[nRadiosCount];
@@ -253,37 +221,10 @@ ON_MOD_LOAD()
         pRadioNames[i]   = cfg->Bind(key, "Untitled", "Names")->GetString();
     }
 
-    nRadioIndex = pCurrentRadioIndex->GetInt();
-    if(nRadioIndex < 0) nRadioIndex = 0;
-
     cfg->Save();
 
-    // hooks
-    HOOKPLT(PauseGame,  pGTASA + BYBIT(0x672644, 0x844230));
-    HOOKPLT(ResumeGame, pGTASA + BYBIT(0x67056C, 0x840CB0));
-    HOOKPLT(StartRadio, pGTASA + BYBIT(0x66F738, 0x83F5C0));
-    HOOK(StopRadio, aml->GetSym(hGTASA, "_ZN20CAERadioTrackManager9StopRadioEP21tVehicleAudioSettingsh"));
-
-    // audio settings hook remove radio option
-    aml->PlaceB(pGTASA + BYBIT(0x2A4D28 + 1, 0x3638A4),
-                pGTASA + BYBIT(0x2A4D3C + 1, 0x3638C0));
-
-    sautils = (ISAUtils*)GetInterface("SAUtils");
-    if(sautils)
-    {
-        sautils->AddSliderItem(
-            SetType_Audio,
-            "Online Radio Volume",
-            pRadioVolume->GetInt(),
-            0,
-            100,
-            SetVolume
-        );
-    }
-
-    // ===================== IMGUI RENDER =====================
     Events::drawHudEvent.after += []()
     {
-        DrawRadioUI();
+        DrawUI();
     };
 }
